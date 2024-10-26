9d1b5148573e4d6aa67e5ae52b0a38b1461418c4
Babos Dávid <babosdavid8@gmail.com>
2024-10-26 12:36:46 +0300

    add motor control driver

---
+++ DCMotor.h
@@ -0,0 +1,32 @@
+/*
+  DCMotor.h - implementation
+  Copyright (c) 2023 Graziano Blasilli.
+*/
+
+
+#ifndef DCMotor_H
+#define DCMotor_H
+
+#include <Arduino.h>
+
+class DCMotor {
+  private:
+    uint8_t pinA;
+    uint8_t pinB;
+    uint8_t pinS;
+  
+  public:
+    // Constructor
+    DCMotor(uint8_t pinA, uint8_t pinB, uint8_t pinS);
+
+    // Turn the motor on with a specified speed.
+    void on(int speed);
+
+    // Turn the motor on with a specified speed, for a given time.
+    void on(int speed, int millisec);
+
+    // Turn the motor off.
+    void off();
+};
+
+#endif
--- camera.ino
+++ camera.ino
@@ -1,44 +1,69 @@
 #include "soc/soc.h"             // disable brownout problems
 #include "soc/rtc_cntl_reg.h"    // disable brownout problems
 
+#include <DCMotor.h>
 
-#include "stream.h"
+#define MOTOR_1_PIN_1   14
+#define MOTOR_1_PIN_2   15
+#define MOTOR_1_PIN_EN  4
+// #define MOTOR_2_PIN_1   13
+// #define MOTOR_2_PIN_2   12
+// #define MOTOR_2_PIN_EN  2
+
+//#include "stream.h"
 
 
-int foo(char variable[32]) {
-  if(!strcmp(variable, "forward")) {
-    Serial.println("Forward");
-  }
-  else if(!strcmp(variable, "left")) {
-    Serial.println("Left");
-  }
-  else if(!strcmp(variable, "right")) {
-    Serial.println("Right");
-  }
-  else if(!strcmp(variable, "backward")) {
-    Serial.println("Backward");
-  }
-  else if(!strcmp(variable, "stop")) {
-    Serial.println("Stop");
-  } else {
-    return -1;
-  }
-  return 0;
-}
+DCMotor motor1 = DCMotor(MOTOR_1_PIN_1, MOTOR_1_PIN_2, MOTOR_1_PIN_EN);
+//DCMotor motor2 = DCMotor(MOTOR_2_PIN_1, MOTOR_2_PIN_2, MOTOR_2_PIN_EN);
 
+// int foo(char variable[32]) {
+//   if(!strcmp(variable, "forward")) {
+//     Serial.println("Forward");
+//   }
+//   else if(!strcmp(variable, "left")) {
+//     Serial.println("Left");
+//   }
+//   else if(!strcmp(variable, "right")) {
+//     Serial.println("Right");
+//   }
+//   else if(!strcmp(variable, "backward")) {
+//     Serial.println("Backward");
+//   }
+//   else if(!strcmp(variable, "stop")) {
+//     Serial.println("Stop");
+//   } else {
+//     return -1;
+//   }
+//   return 0;
+// }
+int dutyCycle = 200;
 void setup() {
   WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector 
   
   Serial.begin(115200);
   Serial.setDebugOutput(false);
+
+
+
+  // initStream();
   
-  initStream();
-  
-  // Start streaming web server
-  startCameraServer(foo);
+  // // Start streaming web server
+  // startCameraServer(foo);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int speed = 100;
+  int t = 1000;
+
+  motor1.on(speed, t);
+  // motor2.on(-speed, t);
+  
+  delay(2000);
+
+  motor1.on(-speed, t);
+  // motor2.on(speed, t);
+
+  delay(2000);
+
 
 }
